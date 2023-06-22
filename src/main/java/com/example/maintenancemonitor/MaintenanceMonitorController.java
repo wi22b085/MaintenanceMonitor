cc9f77adcd930defc2a1f1a4e4d7f662c9ae434e
Alexander Hickelsberger <wi22b085@technikum-wien.at>
2023-06-22 19:02:25 +0200

    add features for reset custom messages to deafault

--- src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MaintenanceMonitorController {
     private String m = "Everything operates as expected";
-    @RequestMapping("/api/message")
-    public String message(@RequestParam String m){
+    @RequestMapping("/api/message/set")
+    public String setMessage(@RequestParam String m){
         if (m==null) {
             m="Everything operates as expected";
         }
         this.m=m;
         return "ok";
     }
+    @RequestMapping("/api/message/reset")
+    public  String reset(){
+        setMessage(null);
+        return "ok";
+    }
+
 }
