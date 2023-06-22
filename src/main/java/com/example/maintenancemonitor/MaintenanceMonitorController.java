303f269326647d07a7a66e85cb873263084afa62
MarcoHudec <marco.hudec3@gmail.com>
2023-06-22 21:54:12 +0200

    create a index starting site with the functions as per User Stories

--- src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitorController.java
@@ -7,6 +7,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MaintenanceMonitorController {
     private String m = "Everything operates as expected";
+
+    @RequestMapping("/")
+    public String showStartsite()
+    {
+        return  "Welcome to the Maintenance Monitor!" +
+                    "<br>" +
+                    "<br>" +
+                    "<br>" +
+                "We are a small hydro-power electricity supplier near Vienna. " +"<br>" +
+                "Our customers expect electricity around the clock with a very high service level agreement."+"<br>" +
+                " It is easy to derive that service times are very important to us." +
+                    "<br>" +
+                    "<br>" +
+                "Type in one of the following options api requests:"+"<br>" +
+                    "<br>" +
+                "-)  /set?a=X ... to set the current status to X"  +
+                    "<br>" +
+                "-)  /setMin?a=X&b=X ... to set the current status using X minutes  and X seconds" +
+                    "<br>" +
+                "-)  /status ... to see the current status level"  +
+                    "<br>" +
+                "-)  /reset ... to reset the current status level";
+
+    }
     @RequestMapping("/api/message/set")
     public String setMessage(@RequestParam String m){
         if (m==null) {
